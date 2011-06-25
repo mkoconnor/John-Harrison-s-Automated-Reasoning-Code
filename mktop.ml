7e97815fe0e29a0d83a6105c75ef562d5f7b2be9
Michael O'Connor <mkoconnor@gmail.com>
2011-06-24 23:29:52 -0400

    delete temp files after done with them

--- mktop.ml
+++ mktop.ml
@@ -15,9 +15,23 @@ let write ~file ~string =
 let () = write ~file:temp_top ~string:top
 let () = write ~file:temp_init ~string:init
 
-let () = Unix.execv temp_top (
-  Array.append [|temp_top|]
-    (Array.append [|"-init"; temp_init|]
-       (Array.sub Sys.argv ~pos:1 ~len:(Array.length Sys.argv - 1))
-    )
-)
+
+let () = 
+  match Unix.fork () with
+    | 0 -> 				(* child *)
+      Unix.execv temp_top (
+	Array.append [|temp_top|]
+	  (Array.append [|"-init"; temp_init|]
+	     (Array.sub Sys.argv ~pos:1 ~len:(Array.length Sys.argv - 1))
+	  )
+      )
+    | _pid -> 				(* parent *)
+      let (_pid, status) = Unix.wait () in
+      (* cleanup *)
+      Unix.unlink temp_top;
+      Unix.unlink temp_init;
+      match status with
+	| Unix.WEXITED i -> exit i
+	| Unix.WSIGNALED i -> Printf.eprintf "signaled %d\n" i; exit 0
+	| Unix.WSTOPPED i -> Printf.eprintf "stopped %d\n" i; exit 0
+
